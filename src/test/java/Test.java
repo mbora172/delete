101d15bae2cb81cf012986be689a1835b32dd6ee
mbora172 <aydogdumb@gmail.com>
2022-07-25 03:45:11 -0400

    f

---
+++ src/test/java/Test.java
@@ -0,0 +1,15 @@
+import org.junit.Assert;
+
+public class Test {
+
+
+    @org.junit.Test
+    public void happy(){
+        System.out.println("something");
+    }
+
+    @org.junit.Test
+    public void derbeder(){
+        Assert.assertTrue(false);
+    }
+}
